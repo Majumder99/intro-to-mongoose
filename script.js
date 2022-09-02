a018aeff3924d2ac5f96474a9e372710e331cc35
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-02 13:38:21 +0600

    added schema and model

--- script.js
+++ script.js
@@ -15,7 +15,54 @@ mongoose.connect(
   }
 );
 
-const user = new User({ name: "Sourav", age: 30 });
-user.save().then(() => {
-  console.log("user saved");
-});
+// const user = new User({ name: "Majumder", age: 33 });
+// user.save().then(() => {
+//   console.log("user saved");
+// });
+
+// const run = async () => {
+//   const user = await User.create({ name: "mahim", age: 23 });
+//   // const user = new User({ name: "Majumder", age: 33 });
+//   // await user.save().then(() => {
+//   //   console.log("user saved");
+//   // });
+//   user.name = "zafrul";
+//   await user.save();
+//   console.log(user);
+// };
+
+// run();
+
+///for new schema
+
+// name: String,
+//   age: Number,
+//   email: String,
+//   createdAt: Date,
+//   updatedAt: Date,
+//   bestFriend: mongoose.SchemaTypes.ObjectId,
+//   hobbies: [String],
+//   address: {
+//     street: String,
+//     city: String,
+//   },
+
+const newRun = async () => {
+  try {
+    // const user = await User.create({
+    //   name: "Mirajul",
+    //   age: 20,
+    //   email: "MajumderJJ@gmail.com",
+    //   hobbies: ["fishing", "eating"],
+    //   address: {
+    //     street: "savar",
+    //     city: "dhaka",
+    //   },
+    // });
+    // console.log(user);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+newRun();
--- user.js
+++ user.js
@@ -1,8 +1,41 @@
 const mongoose = require("mongoose");
 
+const addressSchema = new mongoose.Schema({
+  street: String,
+  city: String,
+});
+
 const userSchema = new mongoose.Schema({
   name: String,
-  age: Number,
+  age: {
+    type: Number,
+    min: 1,
+    max: 100,
+    validate: {
+      validator: (v) => v % 2 === 0,
+      message: (props) => `${props.value} is not an even number`,
+    },
+  },
+  email: {
+    type: String,
+    minLenght: 10,
+    maxLength: 100,
+    required: true,
+    lowercase: true, //for use lowercase for every character automatically, we can use uppercase also
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now(),
+    // default: new Date(),
+  },
+  updatedAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  bestFriend: mongoose.SchemaTypes.ObjectId,
+  hobbies: [String],
+  address: addressSchema,
 });
 
 // user collection in mongodb
